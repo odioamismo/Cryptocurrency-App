5d295d2bcf32f06afc94ca679b2c0b1c868d0224
odioamismo <trojan.kacper00@gmail.com>
2023-07-01 11:13:07 +0200

    Added list of coins on Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import homeStore from "../stores/homeStore";
+import {Link} from "react-router-dom";
 
 const Home = () => {
+
+const store = homeStore()
+
+    useEffect(() => {
+    store.fetchCoins()
+    }, [])
+
     return (
         <div>
-            Home
+            {store.coins.map(coin => {
+                return (
+                    <div key={coin.id}>
+                        <Link to={`/${coin.id}`}>
+                        {coin.name}
+                        </Link>
+                    </div>
+                )
+            })}
         </div>
     );
 };
