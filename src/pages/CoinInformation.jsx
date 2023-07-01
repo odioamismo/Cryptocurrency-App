5cc9a250573a9eaed741a6523da6a7ebad5671ab
odioamismo <trojan.kacper00@gmail.com>
2023-07-01 09:03:06 +0200

    Deleted unnecessary files and added new directories and files

---
+++ src/pages/CoinInformation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CoinInformation = () => {
+    return (
+        <div>
+            CoinInformation
+        </div>
+    );
+};
+
+export default CoinInformation;
