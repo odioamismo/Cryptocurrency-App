bcc40f4d1c53a8f124f52129c4c9b1b1624099c4
odioamismo <trojan.kacper00@gmail.com>
2023-07-01 11:11:34 +0200

    Added list of trending coins

--- src/stores/homeStore.js
+++ src/stores/homeStore.js
@@ -1,17 +1,32 @@
-export const homeStore = {
+import { create } from 'zustand';
+
+const homeStore = create((set) => ({
+    coins: [],
+
     fetchCoins: () => {
         fetch("https://api.coingecko.com/api/v3/search/trending")
             .then((response) => {
                 if (!response.ok) {
-                    return console.error("Request failed")
-                } else {
-                    return response.json()
+                    console.error("Request failed");
+                    return;
                 }
-            }).then(data => {
-                console.log(data)
-        })
-            .catch(err => {
-                console.error(err)
+                return response.json();
             })
+            .then((data) => {
+                const coins = data.coins.map((coin) => ({
+                    name: coin.item.name,
+                    image: coin.item.large,
+                    id: coin.item.id,
+                    price: coin.item.price_btc,
+                    symbol: coin.item.symbol
+                }));
+                set({ coins });
+                console.log(coins);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
     }
-}
+}));
+
+export default homeStore;
