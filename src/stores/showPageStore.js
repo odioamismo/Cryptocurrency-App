a645f15813d82181822d21ba3f8f48a8fad9363a
odioamismo <trojan.kacper00@gmail.com>
2023-07-02 11:55:42 +0200

    Replaced showPageStore.js with showStore.js file

--- src/stores/showPageStore.js
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand';
-
-const showPageStore = create((set) => ({
-
-}));
-
-export default showPageStore;
---
+++ src/stores/showStore.js
@@ -0,0 +1,6 @@
+import { create } from 'zustand';
+
+const showStore = create((set) => ({
+}));
+
+export default showStore;
