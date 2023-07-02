af752c8633e5ae37509fd284647b4d87a97e3fae
odioamismo <trojan.kacper00@gmail.com>
2023-07-02 13:02:00 +0200

    Added an useEffect hook to store data from showStore.js file

--- src/pages/CoinInformation.jsx
+++ src/pages/CoinInformation.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import showStore from "../stores/showStore";
+import {useParams} from "react-router-dom";
 
 const CoinInformation = () => {
+    const store = showStore()
+    const params = useParams()
+
+    useEffect(() => {
+        store.fetchData(params.id)
+    },[])
+
     return (
         <div>
             CoinInformation
