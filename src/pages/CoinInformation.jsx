831784994585e0e0a1a5d2883989b2ee35d9a2e4
odioamismo <trojan.kacper00@gmail.com>
2023-07-08 12:43:50 +0200

    Added data for details

--- src/pages/CoinInformation.jsx
+++ src/pages/CoinInformation.jsx
@@ -87,6 +87,30 @@ const CoinInformation = () => {
                 <Tooltip />
                 <Area type="monotone" dataKey="Price" stroke="#8884d8" fill="#8884d8" />
             </AreaChart>
+            <div>
+                <h4>Market capitalization ranking</h4>
+                <span>{store.data.market_cap_rank}</span>
+            </div>
+            <div>
+                <h4>24h high</h4>
+                <span>${store.data.market_data.high_24h.usd}</span>
+            </div>
+            <div>
+                <h4>24h low</h4>
+                <span>${store.data.market_data.low_24h.usd}</span>
+            </div>
+            <div>
+                <h4>Circulating supply</h4>
+                <span>${store.data.market_data.circulating_supply}</span>
+            </div>
+            <div>
+                <h4>Current price</h4>
+                <span>${store.data.market_data.current_price.usd}</span>
+            </div>
+            <div>
+                <h4>1 year change</h4>
+                <span>{store.data.market_data.price_change_percentage_1y.toFixed(2)}%</span>
+            </div>
         </div>
     );
 };
