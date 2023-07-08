a81348d6bc2abe6ca43ee0bd688f801be3d6b98f
odioamismo <trojan.kacper00@gmail.com>
2023-07-08 15:04:27 +0200

    Added style.scss file and imported into the index.js file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import CoinInformation from "./pages/CoinInformation";
+import "./style.scss"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
