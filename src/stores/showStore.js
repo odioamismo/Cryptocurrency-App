64393c29f1e2c76db137666191c8b4bd294f7672
odioamismo <trojan.kacper00@gmail.com>
2023-07-08 10:48:28 +0200

    Added one more fetch method to show Cryptocurrency details

--- src/stores/showStore.js
+++ src/stores/showStore.js
@@ -1,22 +1,51 @@
 import { create } from 'zustand';
-import axios from "axios";
 
 const showStore = create((set) => ({
+    rechData: [],
+
     fetchData: (id) => {
-        fetch(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=28`)
-            .then((response) => {
-                if (response.ok) {
-                    return response.json();
+        const url1 = `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=28`;
+        const url2 = `https://api.coingecko.com/api/v3/coins/${id}?localization=false&market_data=true`;
+
+        fetch(url1)
+            .then((response1) => {
+                if (response1.ok) {
+                    return response1.json();
                 } else {
-                    console.error("Request failed");
+                    console.error("Request failed")
                 }
             })
-            .then((data) => {
-                console.log(data);
+
+            .then((data1) => {
+                fetch(url2)
+                    .then((response2) => {
+                        if (response2.ok) {
+                            return response2.json();
+                        } else {
+                            console.error("Request failed")
+                        }
+                    })
+                    .then((data2) => {
+                        const rechData = data1.prices.map((price) => {
+                            const [timestamp, prc] = price;
+                            const date = new Date(timestamp).toLocaleDateString('en-us');
+                            return {
+                                Date: date,
+                                Price: prc,
+                            };
+                        });
+
+                        console.log(data2);
+
+                        set({ rechData });
+                    })
+                    .catch((error) => {
+                        console.error('Wystąpił błąd:', error);
+                    });
             })
             .catch((error) => {
-                console.error('Error:', error);
-            });
+            console.error('Wystąpił błąd:', error);
+        });
     }
 }));
 
