0a782477ba4429058c4357312a74b468029584bf
odioamismo <trojan.kacper00@gmail.com>
2023-07-08 12:29:07 +0200

    Changed various names and added new method to update a state

--- src/stores/showStore.js
+++ src/stores/showStore.js
@@ -2,51 +2,38 @@ import { create } from 'zustand';
 
 const showStore = create((set) => ({
     rechData: [],
+    data: null,
 
     fetchData: (id) => {
-        const url1 = `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=28`;
-        const url2 = `https://api.coingecko.com/api/v3/coins/${id}?localization=false&market_data=true`;
+        const marketChartUrl = `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=28`;
+        const detailsUrl = `https://api.coingecko.com/api/v3/coins/${id}?localization=false&market_data=true`;
 
-        fetch(url1)
-            .then((response1) => {
-                if (response1.ok) {
-                    return response1.json();
-                } else {
-                    console.error("Request failed")
-                }
-            })
-
-            .then((data1) => {
-                fetch(url2)
-                    .then((response2) => {
-                        if (response2.ok) {
-                            return response2.json();
-                        } else {
-                            console.error("Request failed")
-                        }
-                    })
-                    .then((data2) => {
-                        const rechData = data1.prices.map((price) => {
-                            const [timestamp, prc] = price;
-                            const date = new Date(timestamp).toLocaleDateString('en-us');
-                            return {
-                                Date: date,
-                                Price: prc,
-                            };
-                        });
-
-                        console.log(data2);
-
-                        set({ rechData });
-                    })
-                    .catch((error) => {
-                        console.error('Wystąpił błąd:', error);
-                    });
+        fetch(marketChartUrl)
+            .then((response) => response.json())
+            .then((rechRes) => {
+                const rechData = rechRes.prices.map((price) => {
+                    const [timestamp, prc] = price;
+                    const date = new Date(timestamp).toLocaleDateString("en-us");
+                    return {
+                        Date: date,
+                        Price: prc,
+                    };
+                });
+                set({ rechData });
             })
             .catch((error) => {
-            console.error('Wystąpił błąd:', error);
-        });
-    }
+                console.error('Error while fetching market_chart:', error);
+            });
+
+        fetch(detailsUrl)
+            .then((response) => response.json())
+            .then((dataRes) => {
+                set({ data: dataRes });
+            })
+            .catch((error) => {
+                console.error('Error while fetching coin data:', error);
+            });
+    },
 }));
 
 export default showStore;
