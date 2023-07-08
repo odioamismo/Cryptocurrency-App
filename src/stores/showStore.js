c84db1f3e765ca95b9657d4b74644273b8b68138
odioamismo <trojan.kacper00@gmail.com>
2023-07-08 09:59:06 +0200

    Added new Fetch method for rechart

--- src/stores/showStore.js
+++ src/stores/showStore.js
@@ -1,8 +1,22 @@
 import { create } from 'zustand';
+import axios from "axios";
 
 const showStore = create((set) => ({
     fetchData: (id) => {
-        console.log("Hello", id)
+        fetch(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=28`)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    console.error("Request failed");
+                }
+            })
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
     }
 }));
 
