7540275341f201189c69673e6b4dc8edc94a6029
odioamismo <trojan.kacper00@gmail.com>
2023-07-10 10:37:34 +0200

    Added new div and spinner icon to input

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,14 @@ const Home = () => {
             <header className="home-search">
                 <div className="width">
                     <h2>Search for a crypto</h2>
-                    <input type="text" value={store.query} onChange={store.setQuery}/>
+                    <div className="home-search-input">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                             width="20"
+                             viewBox="0 0 512 512">
+                            <path fill="currentColor" d="M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z"/>
+                        </svg>
+                        <input type="text" value={store.query} onChange={store.setQuery}/>
+                    </div>
                 </div>
             </header>
             <div className="home-cryptos">
